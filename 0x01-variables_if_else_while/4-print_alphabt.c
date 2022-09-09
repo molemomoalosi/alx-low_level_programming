e43acc71e0aea2dc7240881d138ce3a8352e7150
molemomoalosi <molemo.odirile@gmail.com>
2022-09-09 13:57:06 -0700

    C programming

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *main - Prints alphabet minus q and z
+ *letter - letters to be incrimented
+ *Prints alphabet minus q and z
+ *
+ *Incriment the alphabet from a to z and remove the letters q and z
+ *Return: alphabet minus q and z
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		letter = letter - 'q';
+		putchar(letter);
+	}
+	putchar('\n');
+	return(0);
+}
