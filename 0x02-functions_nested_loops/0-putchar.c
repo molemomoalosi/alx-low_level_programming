a81c8ce91cdc5c6dc9d0b255e9fd4428f1395d01
molemomoalosi <molemo.odirile@gmail.com>
2022-09-13 12:15:43 -0700

    Print

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - prints out putchar
+ *
+ * This function prints out putchar
+ *
+ * Returns: 0
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+
+}
