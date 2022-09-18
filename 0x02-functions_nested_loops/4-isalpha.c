35406f3354f94972ed1b867894a71723b0bab7c1
molemomoalosi <molemo.odirile@gmail.com>
2022-09-18 07:43:27 -0700

    Is alpha function

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -9,19 +9,9 @@
 
 int _isalpha(int c)
 {
-	/*for (c = 'a'; c <= 'z'; c++)
+	for (c = 'a'; c <= 'z'; c++)
 	{
 		return (1);
-	}
-	*/
-
-	if (_isalpha(c) == 1)
-	{
-		return (1);
-	}
-	else if (_isalpha(c) == 0)
-	{
-		return (0);
 	}
 
 	return (0);
