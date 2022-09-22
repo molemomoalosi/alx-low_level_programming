c78eab4326141244ac7f2cc4f26fdce8fbec2d04
molemomoalosi <molemo.odirile@gmail.com>
2022-09-22 16:13:55 -0700

    Copy a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * *_strncpy - copies string
+ * @dest: pointer
+ * @src: other pointer
+ * @n: number
+ *
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+
+	while (i < 0 && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
+	return (dest);
+}
